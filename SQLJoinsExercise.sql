abb4d253a665dcaeb01049d374f95943c5736220
Jordan Segovia <segoviajordan91@gmail.com>
2025-05-17 23:56:26 -0700

    wip: added initial join to fetch product and category names for computers, outlined tasks for sales and review queries

--- SQLJoinsExercise.sql
+++ SQLJoinsExercise.sql
@@ -1,15 +1,28 @@
 /* joins: select all the computers from the products table:
 using the products table and the categories table, return the product name and the category name */
-SELECT P.Name AS Product, C.Name AS Category
+SELECT P.Name AS ProductList, C.Name AS CategoryList
+-- 1
+-- 'SELECT' specifies the colums we want to retrieve in the result.
+-- 'P.Name' refers to the Name column from 'the' 'products' table (using alias 'P')
+-- 'C.Name' refers to the Name column from the 'catergories' table (using alias 'C')
+-- AS is a SQL keyword that make an alias
+-- 'ProductsList' and 'CategoryList' rename the output columns for readability 
 FROM bestbuy.products AS P
+-- 'FROM' is a keyword from SQL its purpose it to define the main table we are querying from, which is 'products' in the 'bestbuy' schema
+-- 'AS' is a keyword to make the alias happen
+-- 'AS P' creates an alias so we can refer to 'products' as 'P' thoughtout the query
 INNER JOIN bestbuy.categories AS C ON P.CategoryID = C.CategoryID
+-- 'INNER JOIN' combines rows from both tables wher there is a match
+-- We are matching rows where 'CategoryID' in 'product' equals 'CategoryID' in 'caterogies'  
+-- AS C' sets an alias for the 'categories' table to keep the query cleaner
 WHERE C.Name = 'Computers';
-
--- I changed 'as' to 'AS'because words like SELECT, FROM, INNER JOING, WHERE and AS are SQL reserved keywords"
--- Acording to lord google, this makes your SQL code easier to read, and is "industry standar" 
+-- WHERE' filters the resutls.
+-- Only rows where ethe caterogy name is 'Computers' will be included in the output
 
  
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+
+
  
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
 
